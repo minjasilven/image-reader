adad9a4a014999ef33596d92d87aa81b5e09c51b
minjasilven <minja.silvennoinen@futurice.com>
2020-10-30 18:28:10 +0200

    WIP adding image splitting

---
+++ ImageReader/ImageReader/ImageSplitter.cpp
@@ -0,0 +1,184 @@
+#include "ImageSplitter.h"
+#include <iostream>
+#include <vector>
+
+/*
+ *		Initialize Settings For Splitting The Image
+ *		- takes image width and height as parameters
+ *		- amount of chunks the image should be sliced into can be calculated using that info
+ */
+void ImageSplitter::initializeSettingsForSplittingTheImage(int widthOfUsedImage, int heightOfUsedImage, unsigned char* pixelsFromUsedImage) {
+	chunkSize = 4;
+
+	totalAmountOfChunks = calculateHowManyChunksAreNeeded(widthOfUsedImage, heightOfUsedImage);
+	totalAmountOfChunksHorizontally = widthOfUsedImage / chunkSize;
+	totalAmountOfChunksVertically = heightOfUsedImage / chunkSize;
+
+	amountOfPixelsInARowOfChunks = widthOfUsedImage * chunkSize;
+	lengthOfOneRowInPixels = widthOfUsedImage;
+	pixelsInOneChunk = chunkSize * chunkSize;
+
+	allPixelsFromImage = pixelsFromUsedImage;
+
+	initializeVectorOfChunks();
+}
+
+/*
+ *		Calculate How Many Chunks Are Needed
+ *		- helper function to get the amount
+ */
+int ImageSplitter::calculateHowManyChunksAreNeeded(int width, int height) {
+
+	int horizontalChunks = width / chunkSize;
+	int verticalChunks = height / chunkSize;
+
+	int amountOfChunks = horizontalChunks * verticalChunks;
+	return amountOfChunks;
+}
+
+/*
+ *		Initialize Vector Of Chunks
+ *		- initializes correct amount of chunks
+ */
+void ImageSplitter::initializeVectorOfChunks() {
+
+	for (int i = 0; i < totalAmountOfChunks; i++) {
+		std::vector<int> chunkOfPixels = initializeArrayOfPixelColorValues();
+
+		PixelChunk tempPixelChunk;
+		tempPixelChunk.colorValueOfPixel = chunkOfPixels;
+
+		allChunks.push_back(tempPixelChunk);
+	}
+}
+
+/*
+ *		Initialize Vector Of PixelData
+ *		- initializes the pixel data (basically color value) stored inside a chunk
+ */
+std::vector<int> ImageSplitter::initializeArrayOfPixelColorValues() {
+
+	std::vector<int> colorValuesOfPixel;
+	for (int i = 0; i < pixelsInOneChunk; i++) {
+		colorValuesOfPixel.push_back(0);
+	}
+
+	return colorValuesOfPixel;
+}
+
+/*
+ *		Start Compression
+ *		- method that handles the compression steps
+ *		- DXT1 (BC1) block compression uses 4x4 pixel blocks (chunks)
+ *		  and compresses their color data
+ */
+void ImageSplitter::startCompression() {
+	sliceImageIntoChunks();
+	// TODO calculate min and max color values of one chunk (and also two intermediate colors)
+	// TODO apply the calculated colors into the chunk and repeat process for all chunks
+	// TODO take all chunks and put them back to the uncharted char array of pixels
+}
+
+/*
+ *		Slice Image Into Chunks
+ *		- image should be sliced into 4x4 pixel chunks
+ *		- one chunk row constains 4 * width of image amount of pixels
+ */
+void ImageSplitter::sliceImageIntoChunks() {
+	int amountOfChunksPopulated = 0;
+
+	// Go through image one chunk row (height of chunk row is chunkSize) at a time
+	for (int i = 0; i < totalAmountOfChunksVertically; i++) {
+		sliceImageIntoOneChunkRow(amountOfChunksPopulated);
+		amountOfChunksPopulated += totalAmountOfChunksHorizontally;
+	}
+}
+
+/*
+ *		Calculate All Pixels Needed For Row Of Chunks
+ *		- find all pixels that are needed to populate one row of chunks
+ *		- one chunk is 4x4, in one row there are widthOfImage/chunkSize number of chunks
+ */
+void ImageSplitter::calculateAllPixelsNeededForRowOfChunks(int startingPoint) {
+
+	for (int i = startingPoint; i < amountOfPixelsInARowOfChunks; i++) {
+		pixelsNeededForOneRowOfChunks.push_back(allPixelsFromImage[i]);
+	}
+}
+
+/*
+ *		Calculate Pixels From One Row Of Image
+ *		- find all pixels that are needed to populate one row
+ *		- one row is 1 pixel * width of an image
+ */
+void ImageSplitter::calculatePixelsFromOneRowOfImage(int startingPoint) {
+	for (int i = startingPoint; i < lengthOfOneRowInPixels; i++) {
+		pixelsOfOneRowOfImage.push_back(pixelsNeededForOneRowOfChunks[i]);
+	}
+}
+
+/*
+ *		Slice Image Into One Chunk Row
+ *		- One chunk row height is chunkSize so total amount of pixels in chunk row
+ *		  is chunkSize * width of an image
+ *		- calculates all pixels needed for one chunk row
+ *		- populates pixels of one row of an image into chunks and repeats it for all
+ *		  rows (amount of rows from image = chunkSize)
+ */
+void ImageSplitter::sliceImageIntoOneChunkRow(int startingPixel) {
+
+	calculateAllPixelsNeededForRowOfChunks(startingPixel);
+
+	currentRowFromChunk = 0;
+	int howManyRepeats = chunkSize;
+
+	for (int i = 0; i < howManyRepeats; i++) {
+		int currentRowStartingPixelPosition = currentRowFromChunk * lengthOfOneRowInPixels;
+
+		calculatePixelsFromOneRowOfImage(currentRowStartingPixelPosition);
+
+		int currentChunkStartingPixelPosition = getCurrentChunkStartingPixelPosition();
+		int maxPixelPositionOnChunk = currentChunkStartingPixelPosition + chunkSize;
+		int currentRowLastPixelPosition = startingPixel + totalAmountOfChunksHorizontally;
+
+		// Save the color value from image into chunk (currently uses temp data)
+		for (int chunk = startingPixel; chunk < currentRowLastPixelPosition; chunk++) {
+			for (int pixelPosition = currentChunkStartingPixelPosition; pixelPosition < maxPixelPositionOnChunk; pixelPosition++) {
+
+				allChunks[chunk].colorValueOfPixel[pixelPosition] = 5;
+			}
+
+			currentChunk++;
+		}
+
+		currentRowFromChunk++;
+	}
+
+	// reset current row of chunk because this set of chunks is added
+	currentRowFromChunk = 0;
+}
+
+/*
+ *		Get Current Chunk Starting Pixel Position
+ *		- hardcoded values, should be refactored
+ *		- returns the starting position of color value saved in chunk in this row
+ */
+int ImageSplitter::getCurrentChunkStartingPixelPosition() {
+
+	int currentPixelFromChunk = 0;
+
+	if (currentRowFromChunk == 0) {
+		currentPixelFromChunk = 0;
+	}
+	else if (currentRowFromChunk == 1) {
+		currentPixelFromChunk = 4;
+	}
+	else if (currentRowFromChunk == 2) {
+		currentPixelFromChunk = 8;
+	}
+	else if (currentRowFromChunk == 3) {
+		currentPixelFromChunk = 12;
+	}
+
+	return currentPixelFromChunk;
+}
---
+++ ImageReader/ImageReader/ImageSplitter.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+
+struct PixelChunk {
+	std::vector<int> colorValueOfPixel;
+};
+
+class ImageSplitter {
+private:
+	/*	These values are set in initialiseSettingsForCompression method	*/
+	int chunkSize;								// chunkSize is 4 because BC1 should use 4x4 blocks of texels
+	int totalAmountOfChunks;					// total amound of chunks in one image
+	int totalAmountOfChunksHorizontally;		// how many chunks there are horizontally in total
+	int totalAmountOfChunksVertically;			// how many chunks there are vertically in total
+	int amountOfPixelsInARowOfChunks;			// how many pixels in total there are in a row of 4x4 chunks
+	int lengthOfOneRowInPixels;					// this is the same as the width of an image
+	int pixelsInOneChunk;						// how many pixels in total are in one chunk (4x4 = 16)
+
+	/*	General helper values	*/
+	int currentPixelFromChunk = 0;
+	int currentPixelFromThisRow = 0;
+	int currentRowFromChunk = 0;
+	int currentChunk = 0;
+
+	std::vector<int> pixelsNeededForOneRowOfChunks;		// one row of chunks is 4 * image width pixels
+	std::vector<int> pixelsOfOneRowOfImage;				// one row of image is image width amount of pixels
+
+	std::vector<PixelChunk> allChunks;
+	unsigned char* allPixelsFromImage;
+
+	/*	Initialization functions	*/
+	void initializeVectorOfChunks();
+	std::vector<int> initializeArrayOfPixelColorValues();
+
+	/*	Helper functions  */
+	int calculateHowManyChunksAreNeeded(int width, int height);
+	void calculateAllPixelsNeededForRowOfChunks(int startingPoint);
+	void calculatePixelsFromOneRowOfImage(int startingPoint);
+	int getCurrentChunkStartingPixelPosition();
+
+
+	void sliceImageIntoOneChunkRow(int startingPoint);
+	void sliceImageIntoChunks();
+
+public:
+	void initializeSettingsForSplittingTheImage(int imageWidth, int imageHeight, unsigned char* pixelsFromImage);
+	void startCompression();
+};
